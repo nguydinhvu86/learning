94d309a801d6bec5dc004867699f740ba4eac7d1
Admin <admin@example.com>
2026-04-18 17:48:20 +0700

    Force add frontend files

---
+++ polyglot-lms-v2/apps/frontend/.next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"app\\\\admin\\\\lesson-builder\\\\[lessonId]\\\\page.tsx -> xlsx\":{\"id\":\"app\\\\admin\\\\lesson-builder\\\\[lessonId]\\\\page.tsx -> xlsx\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_xlsx_xlsx_mjs.js\"]}}"
